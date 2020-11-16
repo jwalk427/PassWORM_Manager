d485a60ef3d220d976e1a80326750174aa487c51
jwalk427 <61562746+jwalk427@users.noreply.github.com>
2020-11-16 16:32:41 -0500

    Fixed wormy

--- src/view/mainView.java
+++ src/view/mainView.java
@@ -120,8 +120,7 @@ public final class mainView extends javax.swing.JFrame {
         desktopPane = new javax.swing.JDesktopPane() {
             public void paintComponent(Graphics g){
                 Graphics2D g2d = (Graphics2D)g;
-                g2d.scale((double)getWidth()/image.getWidth(null), (double)getHeight()/image.getHeight(null));
-                g2d.drawImage(image, 0, 0, this);
+                g2d.drawImage(image, 0, 0, getWidth(), getHeight(), this);
             }
         };
         jPanel1 = new javax.swing.JPanel();
